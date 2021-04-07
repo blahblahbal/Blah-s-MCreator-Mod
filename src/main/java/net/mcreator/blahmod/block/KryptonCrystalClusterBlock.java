142cbd115de961f3ab0bcb823a9274923765cedb
blahblahbal <jabawakijadehawk@gmail.com>
2021-04-06 18:08:12 -0700

    Crystal Caves

---
+++ src/main/java/net/mcreator/blahmod/block/KryptonCrystalClusterBlock.java
@@ -0,0 +1,61 @@
+
+package net.mcreator.blahmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.blahmod.itemgroup.CreativeTabBlahBlocksItemGroup;
+import net.mcreator.blahmod.BlahmodModElements;
+
+@BlahmodModElements.ModElement.Tag
+public class KryptonCrystalClusterBlock extends BlahmodModElements.ModElement {
+	@ObjectHolder("blahmod:krypton_crystal_cluster")
+	public static final Block block = null;
+	public KryptonCrystalClusterBlock(BlahmodModElements instance) {
+		super(instance, 1721);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(CreativeTabBlahBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.SHROOMLIGHT).hardnessAndResistance(0.9f, 0f).setLightLevel(s -> 8)
+					.doesNotBlockMovement().notSolid().setOpaque((bs, br, bp) -> false));
+			setRegistryName("krypton_crystal_cluster");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.LIME;
+		}
+	}
+}
